5fba4e3c681cecaa915b6033178f5d9ebe0fe940
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-10 08:08:29 -0700

    Revert "Removed inputConfig"
    This reverts commit 6af5da67baaef2d46bf443d4d1cf7c31ee1171b4.
    
    Conflicts:
    	packages/ember-easyForm/lib/views/input.js
    	packages/ember-easyForm/tests/helpers/input_test.js

--- packages/ember-easyForm/tests/helpers/input_test.js
+++ packages/ember-easyForm/tests/helpers/input_test.js
@@ -316,6 +316,18 @@ test('does not wrap controls when not defined', function() {
   equal(view.$().find('div.my-wrapper').length, 0, 'should not create the controls wrapper');
 });
 
+test('passes the inputConfig to the input field', function() {
+  view = Ember.View.create({
+    template: templateFor('{{input firstName as=text inputConfig="class:span5;rows:2"}}'),
+    container: container,
+    controller: controller
+  });
+  append(view);
+  var textarea = view.$().find('textarea');
+  equal(textarea.attr('class'), 'ember-view ember-text-area span5');
+  equal(textarea.attr('rows'), '2');
+});
+
 test('sets input attributes property as bindings', function() {
   view = Ember.View.create({
     template: templateFor('{{input firstName placeholderBinding="placeholder" labelBinding="label" hintBinding="hint"}}'),
@@ -364,4 +376,50 @@ test('sets select prompt property as bindings', function() {
   equal(view.$().find('option').text(), controller.get('prompt'));
   equal(view.$().find('label').text(), controller.get('label'));
   equal(view.$().find('.hint').text(), controller.get('hint'));
+});
+
+test('allows to use computed properties defined in the Input view', function() {
+  Ember.EasyForm.Input.reopen({
+    placeholder: function() {
+      return 'MyPlaceholder';
+    }.property(),
+    label: function() {
+      return 'MyLabel';
+    }.property(),
+    hint: function() {
+      return 'MyHint';
+    }.property(),
+    inputConfig: function() {
+      return 'class:MyClass';
+    }.property()
+  });
+
+  view = Ember.View.create({
+    template: templateFor('{{input firstName}}'),
+    container: container,
+    controller: controller
+  });
+  append(view);
+
+  equal(view.$().find('input').prop('placeholder'), 'MyPlaceholder');
+  equal(view.$().find('label').text(), 'MyLabel');
+  equal(view.$().find('.hint').text(), 'MyHint');
+  ok(view.$().find('input').hasClass('MyClass'));
+});
+
+test('allows to override the computed property defined in the Input view', function() {
+  Ember.EasyForm.Input.reopen({
+    label: function() {
+      return 'MyLabel';
+    }.property()
+  });
+
+  view = Ember.View.create({
+    template: templateFor('{{input firstName label="MyFirstName"}}'),
+    container: container,
+    controller: controller
+  });
+  append(view);
+
+  equal(view.$().find('label').text(), 'MyFirstName');
 });
